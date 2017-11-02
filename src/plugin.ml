c1e9ac0d3d2e2c98290c489d6d809cfe8916f181
Aaron L. Zeng <me@bcc32.com>
2017-11-02 16:41:20 -0400

    Flesh out bf interaction plugin

--- src/plugin.ml
+++ src/plugin.ml
@@ -1,4 +1,49 @@
+open Ecaml
+
+let eval_program program input =
+  let program = Value.to_utf8_bytes_exn program in
+  let input   = Value.to_utf8_bytes_exn input   in
+  Bf_lib.Program.run' ~program ~input
+  |> Value.of_utf8_bytes
+;;
+
 let () =
-  Ecaml.message "Hello from OCaml";
-  Ecaml.provide (Ecaml.Symbol.intern "ecaml-bf")
+  defun [%here] (Symbol.intern "bf-eval")
+    ~docstring:"evaluate [program], a brainfuck program, given [input]"
+    ~args:[ Symbol.intern "program"
+          ; Symbol.intern "input" ]
+    (function
+      | [| program; input |] -> eval_program program input
+      | _ -> invalid_arg "wrong arity")
+;;
+
+let eval_current_buffer input =
+  let program =
+    Current_buffer.contents ()
+    |> Text.to_utf8_bytes
+  in
+  let input = Value.to_utf8_bytes_exn input in
+  Bf_lib.Program.run' ~program ~input
+;;
+
+let () =
+  defun [%here] (Symbol.intern "bf-eval-buffer")
+    ~docstring:"evaluate the current buffer as brainfuck code"
+    ~interactive:"sInput: "
+    ~args:[ Symbol.intern "input" ]
+    (function
+      | [| input |] ->
+        let output = eval_current_buffer input in
+        messagef "Output: %s" output;
+        Value.nil
+      | _ -> invalid_arg "wrong arity")
+;;
+
+let () =
+  let keys =
+    Symbol.funcall1 (Symbol.intern "kbd") (Value.of_utf8_bytes "C-x C-j")
+  in
+  Symbol.funcall2_i (Symbol.intern "global-set-key")
+    keys
+    (Value.intern "bf-eval-buffer")
 ;;
