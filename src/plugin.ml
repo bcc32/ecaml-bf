a0254dd29f36a080a9119d1290602299b566d398
Aaron L. Zeng <me@bcc32.com>
2017-11-18 19:00:35 -0500

    Automatically start bf-mode when opening *.b files

--- src/plugin.ml
+++ src/plugin.ml
@@ -39,12 +39,14 @@ let () =
       | _ -> invalid_arg "wrong arity")
 ;;
 
+let mode_name = Symbol.intern "bf-mode"
+
 let () =
   (* define [bf-mode] as a major mode *)
   let mode =
     Major_mode.define_derived_mode ~parent:Major_mode.fundamental
       [%here]
-      ~change_command:(Symbol.intern "bf-mode")
+      ~change_command:mode_name
       ~docstring:"Major mode for interacting with brainfuck code."
       ~initialize:ignore
       ~mode_line:"brainfuck"
@@ -53,6 +55,14 @@ let () =
   let keymap = Major_mode.keymap mode in
   Keymap.define_key keymap (Key_sequence.create_exn "C-x C-e")
     (Command (Command.of_value_exn (Value.intern "bf-eval-buffer")))
+;;
+
+let () =
+  (* automatically start [bf-mode] upon opening a [*.b] file *)
+  Auto_mode_alist.add Auto_mode_alist.Entry.(
+    [ { delete_suffix_and_recur = false
+      ; filename_match = Regexp.of_pattern "\\.b\\'"
+      ; function_ = Some mode_name } ])
 ;;
 
 let () = provide (Symbol.intern "ecaml-bf")
